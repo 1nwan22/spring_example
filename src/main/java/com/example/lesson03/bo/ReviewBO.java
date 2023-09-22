b7e8ee4f68585c969bd41d1322aa74310d4c7fe0
1nwan22 <astralchild220@gmail.com>
2023-09-22 21:41:13 +0900

    230922 3강 mybatis

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -1,15 +1,21 @@
 package com.example.lesson03.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson03.domain.Review;
+import com.example.lesson03.mapper.ReviewMapper;
 
 @Service
 public class ReviewBO {
 	
-	//input:
+	//input: id
 	//output: Review(단건)
-	public Review getReview() {
-		return ;
+	
+	@Autowired
+	private ReviewMapper reviewMapper;
+	
+	public Review getReview(int id) {
+		return reviewMapper.selectReview(id);
 	}
 }
